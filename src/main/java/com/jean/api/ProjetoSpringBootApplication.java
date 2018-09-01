e4e60a1f0710f671274aaba40d7e16adbb971437
Jean <jeancarvalhodosreis@gmail.com>
2018-09-01 17:21:33 -0300

    Alteração no arquivo principal da aplicação.

--- src/main/java/com/jean/api/ProjetoSpringBootApplication.java
+++ src/main/java/com/jean/api/ProjetoSpringBootApplication.java
@@ -8,5 +8,6 @@ public class ProjetoSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+		System.out.println("Projeto Spring Boot");
 	}
 }
